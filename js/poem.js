3ec3b65c2548c0c69c2ff152ff479f046746d40d
Tim Combs <tim00combs@gmail.com>
2023-07-20 14:57:04 -0700

    working version of website sans audio
    deleted files I wasn't using and made edits to css files so map animation would be centered and sized properly. script.js now exits the slide show after the last slide and starts up the map animation. made some edits to the poem.txt, which is still incomplete

--- js/poem.js
+++
@@ -1,6 +0,0 @@
-const audio = new Audio('./poem-test.mp3');
-const playbox = document.getElementById('playbox');
-
-playbox.addEventListener("click", () => {
-  audio.play();
-});
--- js/script.js
+++ js/script.js
@@ -38,43 +38,44 @@ const picArray = [
   './pix/aunt-mildred-unc-elliott.png',
   './pix/dad-baby-tim.png',
   './pix/floyd-dana-tim-who.png',
-  './pix/tim-larry.png',
+  // './pix/tim-larry.png',
   './pix/tim-mom-desert-unknown-car.png',
-  './pix/68-commemoration-drKing-reagan-hater.png',
+  // './pix/68-commemoration-drKing-reagan-hater.png',
   './pix/auntdj-unclegary-carma.png',
   './pix/tim-mom-dad-overlooking-alcatraz.png',
-  './pix/agar-grandparent-house-newton-iowa.png',
-  './pix/tim-mom-dad-overlooking-alcatraz.png',
+  // './pix/agar-grandparent-house-newton-iowa.png',
   './pix/ma-tim-aunt-dora-grandpa-floyd-etc.png',
-  './pix/tim-neighbor-kids.png',
+  // './pix/tim-neighbor-kids.png',
   './pix/dad-lloyd-barbers.png',
-  './pix/golden-gate-bridge.png',
-  './pix/tim-mom-no-baby-seat-utah.png',
+  // './pix/golden-gate-bridge.png',
+  // './pix/tim-mom-no-baby-seat-utah.png',
   './pix/mom-dad-turner-falls-park-oklahoma.png',
   './pix/grandparalph.png',
   './pix/granpaphil-grandmaruth.png',
-  './pix/interracial-couple.png',
+  // './pix/interracial-couple.png',
   './pix/tim-sabrina.png',
-  './pix/ma-writing-packing.png',
+  // './pix/ma-writing-packing.png',
   './pix/mom-dad-des-moines.png',
   './pix/delmar-sammy-uncle-lloyd-tim-etc.png',
-  './pix/mom-lake-texarkana.png',
-  './pix/lil-lloyd-claudia.png',
-  './pix/mom-near-stinson-beach.png',
+  // './pix/mom-lake-texarkana.png',
+  // './pix/lil-lloyd-claudia.png',
+  // './pix/mom-near-stinson-beach.png',
   './pix/mom-tim-j-el-moro-puerto-rico.png',
-  './pix/mormon-tabernacle-salt-lake.png',
+  // './pix/mormon-tabernacle-salt-lake.png',
   './pix/sacramento-hippies-tim-mom.png',
   './pix/tim-kim-first-birthday-ogden.png',
   './pix/tim-john-muir-first-grade-fresno.png',
   './pix/tim-mom-christmas-mojave-desert.png',
   './pix/tim-mom-alphonso-tombstone.png',
-  './pix/tim-mom-others-victorville.png',
-  './pix/tim-mom-dad-land-park-sacramento.png',
+  // './pix/tim-mom-others-victorville.png',
+  // './pix/tim-mom-dad-land-park-sacramento.png',
   './pix/tim-pool.png',
 ]
 
 const pic = document.getElementById('pic');
+const map = document.getElementById('map');
 const picHolder = document.getElementById('pic__holder');
+const mapHolder = document.getElementById('map__holder');
 let lnth = picArray.length;
 
 async function someProcedure (n) {
@@ -88,9 +89,16 @@ async function someProcedure (n) {
   }
 
   // this resets the slide show to the beginning once you hit the final pic
+  // or it moves to the map animation
   // note the recursion!
   if (i = n) {
-    i = 0;
+    // this line is for if you want to reset to final pic
+    // i = 0;
+
+    // this code is for moving to the map animation
+    picHolder.style.opacity = 0;
+    mapHolder.style.opacity = 1;
+    map.src = 'map.gif';
   }
   someProcedure(lnth);
   // return 'done';
@@ -103,7 +111,7 @@ async function someProcedure (n) {
 //      NOTE: clicking image starts spinning pix as well
 //            clicking image makes it disappear
 // *************************************************************
-const boxHolder = document.getElementById('box-holder');
+const boxHolder = document.getElementById('box__holder');
 const audio = new Audio('./poem-test.mp3');
 const playbox = document.getElementById('playbox');
 
